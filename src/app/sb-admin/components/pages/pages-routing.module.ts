546b11e7be487fba7ae823e5b87cbb1685b16c8a
Gourav More <gourav_m@tekditechnologies.com>
2023-04-07 11:30:06 +0530

    Issue #196427 feat: Skeleton for Admin panel

--- src/app/sb-admin/components/pages/pages-routing.module.ts
+++ src/app/sb-admin/components/pages/pages-routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [RouterModule.forChild([
         { path: 'sb-user', loadChildren: () => import('./sb-user/sbuser.module').then(m => m.SbUserModule) },
-        { path: 'empty', loadChildren: () => import('./empty/emptydemo.module').then(m => m.EmptyDemoModule) },
         { path: '**', redirectTo: '/notfound' }
     ])],
     exports: [RouterModule]
