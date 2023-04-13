a33a822134ab44d725dfaebd95042b82a6ceaa55
poojakarma <pooja.karma@tekditechnologies.com>
2023-04-13 17:10:59 +0530

    update sb-user-management.module

--- projects/sb-user-management/src/lib/sb-user-management.module.ts
+++ projects/sb-user-management/src/lib/sb-user-management.module.ts
@@ -2,20 +2,16 @@ import { NgModule } from '@angular/core';
 import { SbUserManagementComponent } from './sb-user-management.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { TabViewModule } from 'primeng/tabview';
-import { UserCountComponent } from './user-count/user-count.component';
-import { DataViewModule } from 'primeng/dataview';
-import { TableModule } from 'primeng/table';
+
+
 
 @NgModule({
   declarations: [
     SbUserManagementComponent,
-    UserListComponent,
-    UserCountComponent
+    UserListComponent
   ],
   imports: [
-    TabViewModule,
-    DataViewModule,
-    TableModule 
+    TabViewModule
   ],
   exports: [
     SbUserManagementComponent
