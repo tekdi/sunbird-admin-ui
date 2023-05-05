4b5baeba488e37073bd071e2d537323481faebb6
poojakarma <pooja.karma@tekditechnologies.com>
2023-05-05 19:16:38 +0530

    Removed i18next code from dashboard module

--- src/app/sb-admin/components/dashboard/dashboard.module.ts
+++ src/app/sb-admin/components/dashboard/dashboard.module.ts
@@ -12,44 +12,8 @@ import { DashboardsRoutingModule } from './dashboard-routing.module';
 import { InputTextModule } from 'primeng/inputtext';
 import { MessagesModule } from 'primeng/messages';
 import { MessageModule } from "primeng/message";
-import { APP_INITIALIZER, LOCALE_ID } from '@angular/core';
-import { I18NextModule, ITranslationService, I18NEXT_SERVICE, defaultInterpolationFormat } from 'angular-i18next';
-import en from "src/locales/en/en.json";
+import { I18NextModule} from 'angular-i18next';
 
-export function appInit(i18next: ITranslationService) {
-    let lang = localStorage.getItem('lang');
-    const data = i18next.init({
-        fallbackLng: lang ? lang : "en",
-        debug: true,
-        returnEmptyString: false,
-        resources: {
-            en: {
-                translation: en
-            }
-        },
-        ns: ['translation'],
-        defaultNS: 'translation',
-        interpolation: {
-            format: I18NextModule.interpolationFormat(defaultInterpolationFormat),
-        },
-    });
-    return () => data;
-}
-export function localeIdFactory(i18next: ITranslationService) {
-    return i18next.language;
-}
-export const I18N_PROVIDERS = [
-    {
-        provide: APP_INITIALIZER,
-        useFactory: appInit,
-        deps: [I18NEXT_SERVICE],
-        multi: true
-    },
-    {
-        provide: LOCALE_ID,
-        deps: [I18NEXT_SERVICE],
-        useFactory: localeIdFactory
-    }];
 
 @NgModule({
     imports: [
@@ -66,9 +30,6 @@ export const I18N_PROVIDERS = [
         MessagesModule,
         MessageModule,
         I18NextModule.forRoot(),
-    ],
-    providers: [
-        I18N_PROVIDERS
     ],
     declarations: [DashboardComponent]
 })
