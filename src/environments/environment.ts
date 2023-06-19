06313c99c908f6c40947f33efd2606b6dd3cc509
snehalsabade <snehal.sabade@tekditechnologies.com>
2023-06-19 23:51:55 +0530

    change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,20 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  port: "http://localhost:4200/",
-  instance: "https://sunbirdsaas.com/",
-  apiUrl: "https://sunbirdsaas.com/api",
-  target: "https://sunbirdsaas.com",
-  addOrgUrl: "api/org/v1/create",
-  orgUrl:"api/org/v1/search",
-  userUrl : "api/user/v1/search",
-  userCreateUrl:"api/user/v3/create",
-  userUpdateRoleUrl:"api/user/v1/role/assign",
-  blockUserUrl:"api/user/v1/block",
-  unblockUserUrl:"api/user/v1/unblock",
-  authKey : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJXTFBhVTZTY0cyejFZMng4Y0FWNDJMVlRscUhMekV0YyJ9.7fhXiT3Mhbu7PT2PITYyF2tdW8ZowYJYnzpK5yGgHqc",
-  userToken : "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJTS1pUNEh5WWlaeHppTnJJZFVrSy1yaWY1QXJVYkVDODRsWGoyeVB3R2pJIn0.eyJqdGkiOiI0ZjU0ZTAyNi0xMjVhLTQxZTYtYjc5Ny1jZmU1ZjM2M2U1OGUiLCJleHAiOjE2ODU3MDEwMDEsIm5iZiI6MCwiaWF0IjoxNjg1Njc5NDAxLCJpc3MiOiJodHRwczovL3N1bmJpcmRzYWFzLmNvbS9hdXRoL3JlYWxtcy9zdW5iaXJkIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6ImY6ZTRmZDJjZjctMWIxOC00NWNmLWIwYjMtNTVjYzNlZGVhNTMzOjllZjY4ZDk1LTdkODAtNDBmYi1iNDc4LWQ1ZmY3OTQ2ODViOSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImltcGxlbWVudGF0aW9uIiwiYXV0aF90aW1lIjowLCJzZXNzaW9uX3N0YXRlIjoiMzMyYzgyMWUtNDE0Yi00NTE5LThmYjItMTA4ODg1NTI5ZWEyIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL3N1bmJpcmRzYWFzLmNvbSJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiY3JlYXRlLXJlYWxtIiwib2ZmbGluZV9hY2Nlc3MiLCJhZG1pbiIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoiU2hhc2hpa3VtYXIgUCBQIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiZGhmb19zaGFzaGlrdW1hciBwIiwiZ2l2ZW5fbmFtZSI6IlNoYXNoaWt1bWFyIFAiLCJmYW1pbHlfbmFtZSI6IlAiLCJlbWFpbCI6InNoKioqKioqKioqKioqKioqQGdtYWlsLmNvbSJ9.VZidOBVnBNc2xapzq35iNB8x52gWNEshoKHWsIQzRDQjkJhR_3BjIsUUKi5ZGoJvMpDLDdXOD9JXc5-rsBfp_5Kx4yksK6BLg5HP9Bh3Y_T_0IC6swppoPt3ka2fv_BEFk8a3f2RuTxgHS0CUTul63ti8oXXX5Equ0-gLM7oZg6-xOaO2YqSnmo5Ijp-Xins2ZD_WNapuFdozvFxSRL9xbPj1FBW98Hw4MA4qGN4HhmJEPCnW4PW4jrgxYbH12Rpn8OIIVpqwEh-91Om9xgjVdxY6OfhX3tycxQjMmHKSorYJXH7JEjWPYZ_bL9n4D8w8kOBR1Y_QOP2oVqTr5ASTA"
+  production: false
 };
 
 /*
