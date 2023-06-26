d49c9d76b68a2f60d413124c0971078ce5dbdd19
snehalsabade <snehal.sabade@tekditechnologies.com>
2023-06-26 09:24:58 +0530

    change

--- src/app/sb-admin/components/pages/sb-dashboard/sb-dashboard-routing.module.ts
+++ src/app/sb-admin/components/pages/sb-dashboard/sb-dashboard-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SbDashboardComponent } from './sb-dashboard.component';
-
-// const routes: Routes = [];
-
 @NgModule({
   imports: [RouterModule.forChild(
     [{ path: '', component: SbDashboardComponent }
