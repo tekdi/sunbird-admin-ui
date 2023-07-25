5a2ca5b34e9ab71da7f68deb16f8b35786c8bbf9
snehalsabade <132333617+snehalsabade@users.noreply.github.com>
2023-07-25 12:27:09 +0530

    #199988 user dashboard (#20)
    * skeleton created for user dashboard
    
    * showing useName and userRole count
    
    * change
    
    * urls added in config file
    
    * to myTable to sbDashboardTable
    
    * change
    
    * change
    
    * translate menu
    
    * change

--- src/app/sb-admin/components/pages/pages-routing.module.ts
+++ src/app/sb-admin/components/pages/pages-routing.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
     imports: [RouterModule.forChild([
         { path: 'sb-user', loadChildren: () => import('./sb-user/sbuser.module').then(m => m.SbUserModule) },
         { path: 'sb-organization', loadChildren: () => import('./sb-organization/sb-organization.module').then(m => m.SbOrganizationModule) },
+        { path: 'user-dashboard', loadChildren: () => import('./user-dashboard/user-dashboard.module').then(m => m.UserDashboardModule) },
         { path: '**', redirectTo: '/notfound' }
     ])],
     exports: [RouterModule]
