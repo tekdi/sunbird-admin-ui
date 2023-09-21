797a6af40d16bdf635759612f7b4aca8355179fe
Deepali Lodhi <114398229+Deepali-1508@users.noreply.github.com>
2023-09-21 23:24:04 +0530

    Update BulkUpload.tsx

--- bulk-upload/BulkUpload.tsx
+++ bulk-upload/BulkUpload.tsx
@@ -1,15 +1,1 @@
-import * as React from 'react'
 
-import { useState } from 'react'
-import {Button} from '@deepalilodhi/react-bulk-upload-1'
-
-
-const BulkUpload = () =>{
-    return (
-        <div>
-            <Button buttonName={'Upload'} uploadUrl={"/bulkupload/ops-tool-upload"} cookie={''} reviewerCookie={''}/>
-        </div>
-    )
-}
-
-export default BulkUpload;
